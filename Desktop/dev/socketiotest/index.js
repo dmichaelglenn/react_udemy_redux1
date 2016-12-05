3e5a5d419ee9e478649cbfebd2405510a7291ca9
Michael Glenn <dmichaelglenn@gmail.com>
2016-12-05 12:01:34 -0800

    more functions

--- Desktop/dev/socketiotest/index.js
+++ Desktop/dev/socketiotest/index.js
@@ -12,6 +12,13 @@ io.on('connection', function(socket){
   console.log('a user connected');
 });
 
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
