637db6698f20605414651e60b43a87ede4d809d3
Michael Glenn <dmichaelglenn@gmail.com>
2016-12-27 14:12:44 -0800

    conditional rendering

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function () {
     return [
-        { title: 'Javascript: The Good Parts' },
-        { title: 'Harry Potter' },
-        { title: 'The Dark Tower' },
-        { title: 'Eloquent Javascript' }
+        { title: 'Javascript: The Good Parts', pages: 101 },
+        { title: 'Harry Potter', pages: 202 },
+        { title: 'The Dark Tower', pages: 6 },
+        { title: 'Eloquent Javascript', pages: 800 }
     ]
 }
